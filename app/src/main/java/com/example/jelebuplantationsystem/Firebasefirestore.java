58e8f671dd2f548f2fa7487e45863cca0330f618
NUR SYAZNEE BINTI SIES <syazneesies@gmail.com>
2020-08-10 20:33:36 +0800

    Revert "Drawer Fragment, Profile Picture, Edit Profile Picture"
    This reverts commit cc4810a15a4b485e066255eac96d972459782ae2.

--- app/src/main/java/com/example/jelebuplantationsystem/Firebasefirestore.java
+++
@@ -1,9 +0,0 @@
-package com.example.jelebuplantationsystem;
-
-public class Firebasefirestore {
-    public static Firebasefirestore getInstane() {
-    }
-
-    public Object collection(String users) {
-    }
-}
