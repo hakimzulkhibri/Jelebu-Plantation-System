5b3e9e02901778ff7ba15e6751d723de2c213535
NUR SYAZNEE BINTI SIES <syazneesies@gmail.com>
2020-09-03 13:08:08 +0800

    User Home is done, Card view can be click and take to another class, Admin edit profile and Admin Profile, Background every class has been added.

---
+++ app/src/main/java/com/example/jelebuplantationsystem/DataReport.java
@@ -0,0 +1,14 @@
+package com.example.jelebuplantationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_report);
+    }
+}
---
+++ app/src/main/java/com/example/jelebuplantationsystem/Timestamp.java
@@ -0,0 +1,14 @@
+package com.example.jelebuplantationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Timestamp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timestamp);
+    }
+}
