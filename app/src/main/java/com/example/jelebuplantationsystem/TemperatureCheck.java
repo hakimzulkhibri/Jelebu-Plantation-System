039af3810720f33407faffb5170f717242f7fad7
NUR SYAZNEE BINTI SIES <syazneesies@gmail.com>
2020-10-02 10:14:34 +0800

    Admin View List User

--- app/src/main/java/com/example/jelebuplantationsystem/TemperatureCheck.java
+++ app/src/main/java/com/example/jelebuplantationsystem/TemperatureCheck.java
@@ -1,17 +1,15 @@
 package com.example.jelebuplantationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 
 public class TemperatureCheck extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_check);
-    }
 
+    }
 }
